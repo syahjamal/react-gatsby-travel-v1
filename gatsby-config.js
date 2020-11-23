7fd7a0ef8a396ac451b9845eed7ed4fde5bcc158
syahjamal <sulaimansyahjamal@gmail.com>
2020-11-23 17:15:08 +0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Test Gatsby Javascript`,
+    description: `Travel website showcasing the best travel destination
+    and deals online`,
     author: `@gatsbyjs`,
   },
   plugins: [
